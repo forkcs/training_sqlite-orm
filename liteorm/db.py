996f843035de6eaf2f13f11cf7f12eea4fcec347
Fedor Soldatkin <fsoldatkin@yandex.ru>
2020-05-04 17:15:06 +0300

    Write simple orm functionality.
    Features:
        - Create/Drop tables
        - Insert rows to a table
        - Manual commit transactions

--- liteorm/db.py
+++ liteorm/db.py
@@ -1,89 +1,52 @@
 import sqlite3
 import logging
-from typing import Tuple
-
-from liteorm.tables import Table
 
 
-class DataBase:
+class Database:
 
-    def __init__(self, data_base_path=None) -> None:
-        self.data_base_path = data_base_path or ':memory:'
-        self.connect = sqlite3.connect(self.data_base_path)
-        self.cursor = self.connect.cursor()
+    def __init__(self, db_path: str = ':memory:') -> None:
+        self.conn = sqlite3.connect(db_path)
+        self.cursor = self.conn.cursor()
 
+        logging.basicConfig()
         self.logger = logging.getLogger('liteorm')
         self.logger.setLevel(logging.DEBUG)
 
-    def create_table(self, table: Table, recreate=False) -> None:
-        if recreate:
-            self.drop_table(table)
+    def save(self) -> None:
+        self.conn.commit()
 
-        prepared_columns = table.columns_to_insert
-        sql = ','.join(prepared_columns)
+    def exit(self):
+        self.cursor.close()
+        self.conn.close()
+
+    ##########
+    # TABLES #
+    ##########
+
+    def create_table(self, table, soft=True) -> None:
+        """Create table by Table object"""
+
         try:
-            self.cursor.execute(f'CREATE TABLE {table} ({sql})')
+            query = table.sql_string_create()
+            self.logger.debug(f'Executing query: {query}')
+            self.cursor.execute(query)
         except sqlite3.OperationalError:
-            self.logger.warning(f'Table {table} already exists.')
-        else:
-            self.logger.info(f'Table {table} successfully created.')
+            if soft:
+                return
+            else:
+                self.drop_table(table)
+                self.create_table(table)
 
-    def drop_table(self, table: Table) -> None:
-        self.cursor.execute(f'DROP TABLE {table}')
-        self.logger.info(f'Table {table} successfuly deleted.')
+    def drop_table(self, table) -> None:
+        query = table.sql_string_drop()
+        self.logger.debug(f'Executing query: {query}')
+        self.cursor.execute(query)
 
-    def insert(self, rows: Tuple[Table]) -> None:
-        for row in rows:
-            try:
-                columns = []
-                values = []
-                for column in row.columns_names:
-                    columns.append(column)
-                    values.append(row.values[column])
-                columns = ','.join(['?' for _ in columns])
+    ########
+    # Data #
+    ########
 
-                sql = f'INSERT INTO {row} VALUES ({columns})'
-                self.cursor.execute(sql, values)
-                self.logger.info(f'Record successfully inserted to {row}.')
-            except sqlite3.IntegrityError as exc:
-                self.logger.warning(f'Record already exists or "{exc}".')
-                continue
-
-        self.connect.commit()
-
-    def update(self, schema, set_, where) -> None:
-        sql_set = [f"{column}={value!r}" for column, value in set_.items()]
-        sql_set = f"\n SET " + ', '.join(map(str, sql_set))
-
-        sql_where = [f"{column}={value!r}" for column, value in where.items()]
-        sql_where = f"\n WHERE " + ' and '.join(map(str, sql_where))
-
-        sql = f'UPDATE {schema}' + sql_set + sql_where
-
-        self.cursor.execute(sql)
-        self.connect.commit()
-        self.logger.info(f'Record {where} updated to {set_} from {schema} table.')
-
-    def delete(self, table: Table, where_param) -> None:
-        sql_query = [f'{column}={value!r}' for column, value in where_param.items()]
-        sql_where = f'WHERE ' + ' and '.join(map(str, sql_query))
-        sql = f'DELETE FROM {table} ' + sql_where
-        self.cursor.execute(sql)
-        self.connect.commit()
-        self.logger.info(f'Record {where_param} deleted from {table} table.')
-
-    def select(self, schema, join_schema=None, where_param=None) -> None:
-        where_param = where_param or {1: 1}
-
-        sql_where = [f"{column}={value!r}" for column, value in where_param.items()]
-        sql_where = f"\n WHERE " + ' and '.join(map(str, sql_where))
-        sql_join = ''
-
-        if join_schema:
-            fk = schema.get_foreign_key()
-            sql_join = f'\n JOIN {join_schema} on {schema}.{fk[0]}={join_schema}.id'
-
-        sql = f"SELECT * FROM {schema}\n" + sql_join + sql_where
-
-        self.cursor.execute(sql)
-        self.logger.debug(self.cursor.fetchall())
+    def add_row(self, table, row: tuple):
+        query = table.sql_string_add_raw(row=row)
+        self.logger.debug(f'Executing query: {query}')
+        self.cursor.execute(query)
--- liteorm/tables.py
+++ liteorm/tables.py
@@ -1,41 +1,37 @@
 class Table:
-    __table_name__ = ''
 
-    id = ('INTEGER', 'PRIMARY KEY', 'AUTOINCREMENT')
+    @classmethod
+    def _get_name(cls):
+        return cls.__name__.lower()
 
-    def __init__(self, **kwargs) -> None:
-        self._values = kwargs
-        if 'id' not in kwargs:
-            self._values['id'] = None
+    @classmethod
+    def _columns(cls):
+        columns = []
+        for k, v in cls.__dict__.items():
+            if not k.startswith('__'):
+                columns.append(f'{k} {" ".join(v)}')
+        return ', '.join(columns)
 
-    @staticmethod
-    def _is_field(key: str) -> bool:
-        if key.startswith('__'):
-            return False
-        if key.endswith('__'):
-            return False
-        return True
+    @classmethod
+    def _column_names(cls):
+        names = [k for k in cls.__dict__.keys() if not k.startswith('__')]
+        return ', '.join(names)
 
-    @property
-    def columns_to_insert(self):
-        field_names = [f'{k} {" ".join(v)}' for k, v in self.__class__.__dict__.items()
-                       if self._is_field(k)]
-        return field_names
+    @classmethod
+    def sql_string_create(cls) -> str:
+        name = cls._get_name()
+        columns = cls._columns()
+        sql_string = f'CREATE TABLE {name} ({columns})'
+        return sql_string
 
-    @property
-    def columns_names(self):
-        field_names = [f'{k if k != "key" else ""}' for k in self.__class__.__dict__.keys()
-                       if self._is_field(k)]
-        return field_names
+    @classmethod
+    def sql_string_drop(cls) -> str:
+        sql_string = f'DROP TABLE {cls._get_name()}'
+        return sql_string
 
-    @property
-    def values(self):
-        return self._values
+    @classmethod
+    def sql_string_add_raw(cls, row: tuple) -> str:
+        str_row = ', '.join(row)
+        sql_string = f'INSERT INTO {cls._get_name()} ({cls._column_names()}) VALUES ({str_row})'
+        return sql_string
 
-    def get_foreign_key(self):
-        field_names = [f'{column}' for column, value in self.__class__.__dict__.items()
-                       if self._is_field(column) and 'REFERENCES' in value]
-        return field_names
-
-    def __str__(self):
-        return self.__table_name__
